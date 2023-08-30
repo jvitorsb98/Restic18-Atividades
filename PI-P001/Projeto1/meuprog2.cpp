2ec0556b35be9d801c9caca1eb08352f13160609
jvitorsb98 <jvitorsb98@outlook.com>
2023-08-30 10:52:34 -0300

    Exercicio do 1 ao 4

---
+++ PI-P001/Projeto1/meuprog2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int A;
+    int B;
+
+    cout << "A = ";
+    cin >> A;
+
+    cout << "B = ";
+    cin >> B;
+
+    cout << endl << "Soma = " << A+B << endl;
+    cout << "Subtração = " << A-B << endl;
+    cout << "Multiplicação = " << A*B << endl;
+    cout << "Divisão = " << A/B << endl;
+    cout << "Resto = " << A%B << endl;
+
+
+    return 0;
+}
